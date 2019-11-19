88d0bbc98838193e8214e2d957ed6a3ab2eb8248
FelipeVM00 <dvargas00@uan.edu.co>
2019-11-19 00:19:38 -0500

    Añado ingresos & negocios

--- EFIP/app/src/main/java/com/uan/efip/ui/tools/ToolsFragment.kt
+++ EFIP/app/src/main/java/com/uan/efip/ui/tools/ToolsFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -14,18 +14,26 @@ class ToolsFragment : Fragment() {
 
     private lateinit var toolsViewModel: ToolsViewModel
 
+    val types = arrayOf("Bogotá", "Medellin", "Cali", "Sta Marta", "Tunja", "Villavicencio", "Otra...")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        toolsViewModel =
-            ViewModelProviders.of(this).get(ToolsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_tools, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tools)
-        toolsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        val t=inflater.inflate(R.layout.fragment_tools, container, false)
+        val spinner = t.findViewById<Spinner>(R.id.spinner)
+        spinner?.adapter = ArrayAdapter(this.activity!!, R.layout.support_simple_spinner_dropdown_item, types) as SpinnerAdapter
+        spinner?.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("Error")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val type = parent?.getItemAtPosition(position).toString()
+                //  Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
+                println(type)
+            }
+
+        }
+        return t
     }
 }
